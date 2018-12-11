70028f3b72ba30854cfb02a6bb459a462dae81c8
Morgan M <morgan.maxwell20@gmail.com>
2018-12-11 12:23:38 -0700

    ready for moment.js to set up math for times

--- logic.js
+++ logic.js
@@ -1,26 +1,46 @@
 $(document).ready(function () {
-
+    // declaring a shortcut for this call
     var database = firebase.database();
-
-    function dataPusher() {
-
-    };
+    // on click event to grab userInput and push it to the database
     $('#submit').on('click', function () {
-        console.log("test");
-        var newTableRow = $('<tr>');
+        // userInputs
         var trainName = $('#trainName').val().trim();
         var destination = $('#destination').val().trim();
         var firstTime = $('#firstTime').val().trim();
         var frequency = $('#frequency').val().trim();
+        // push to firebase using object notation
         database.ref().push({
             trainName: trainName,
             destination: destination,
             firstTime: firstTime,
-            frequency: frequency
+            frequency: frequency,
         });
-        // var minutestonexttrain = math TODO
     });
-    console.log("test");
-    database.on("value", dataPusher);
+    // wait for new data to be put in the database
+    database.ref().on("child_added", function (childSnapshot) {
+        // making the next row of data
+        var newTableRow = $('<tr>');
+        // these are to gather the data from the database;
+        var trainNameData = childSnapshot.val().trainName;
+        var destinationData = childSnapshot.val().destination;
+        var frequencyData = childSnapshot.val().frequency;
+        // this is going to get complicated with this math
+        var nextArrivalData = "TODO";
+        var minutesAwayData = "TODO";
+        // this is to put the database data in a <td> element
+        var trainName = $('<td>').text(trainNameData);
+        var destination = $('<td>').text(destinationData);
+        var frequency = $('<td>').text(frequencyData);
+        var nextArrival = $('<td>').text(nextArrivalData);
+        var minutesAwayData = $('<td>').text(minutesAwayData);
+        // this is to put all of that data into the table row
+        newTableRow.append(trainName);
+        newTableRow.append(destination);
+        newTableRow.append(frequency);
+        newTableRow.append(nextArrival);
+        newTableRow.append(minutesAway)
+        // finally, push the row to the page
+        $('#tableData').append(newTableRow);
+    });
 });
 
